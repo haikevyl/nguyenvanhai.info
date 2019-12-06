9d2d5dafdfed06ed4baa121de00e6351c89fee30
haikevyl <57405193+haikevyl@users.noreply.github.com>
2019-12-06 11:50:06 +0700

    Update index.js

--- menu/index.js
+++ menu/index.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 	$(".menu li a").each(function(index) {
 		$(this).append('<span class="shadow">'+ $(this).text() +'</span>');
 		$(this).hover(function(event) {
-			event.stopPropagation();
 			$(this).animate({
 				top: "-50px"
 			},200);
 		}, function() {
+			$(this).stop();
 			$(this).animate({
 				top: "0"
 			},200);
