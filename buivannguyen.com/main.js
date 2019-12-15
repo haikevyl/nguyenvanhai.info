2fedf90126954d2b8cb7fcd9a5f9a0222f678596
= <=>
2019-12-15 11:32:36 +0700

    add

--- buivannguyen.com/main.js
+++ buivannguyen.com/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-	
+	AOS.init({
+		easing: 'ease-in-out-sine'
+	});
 	// Scroll function
 	$(document).scroll(function(event) {
+
 		var bodyScroll = $("html,body").scrollTop();
 		if (bodyScroll > 20) {
 			$("#nav").addClass('nav-active');
 		}else{
 			$("#nav").removeClass('nav-active');
-		}
+		};
 	});
 
 	$("#toggle-menu").click(function(event) {
@@ -15,5 +18,12 @@ $(document).ready(function() {
 	});
 	$("#close-menu").click(function(event) {
 		$(".main-menu").removeClass('main-menu-active');
+	});
+
+	$(".link-scroll").click(function(event) {
+		event.stopPropagation();
+		var idElements = $(this).attr("href");
+		var offsetTop = $(idElements).offset().top;
+		$('html,body').animate({scrollTop: offsetTop},600);
 	});
 });
